362dbde14d7240238349e8fd9076f6de34d37524
Roberto Arcomano <info@robertoarcomano.com>
2023-04-30 14:58:28 +0200

    Polymorphism. ok

--- 1 Python-Advanced-1/1.2 OOP-Advanced/1.2.2 Inheritance and Polymorphism.py
+++ 1 Python-Advanced-1/1.2 OOP-Advanced/1.2.2 Inheritance and Polymorphism.py
@@ -1,4 +1,35 @@
 """
-1. Multiple inhreritance: MRO problem (conflict between left first rule and specialisation rule)
+1. Multiple inhreritance: MRO problem (conflict between left first rule and specialisation rule.
+    It happens when you select a base class before a more specialised one)
+2. Polymorphism:
+    2.1. Inheritance (same method name in different classes in the same inheritance tree)
+    2.2. Duck typing (only some of the classes can manage a specific method -> otherwise exception)
+         It is more generic than simple inheritance
 """
-class Myclass
+def polymorphism():
+    a = 10
+    print(a.__add__(20))
+    a = "Hello"
+    print(a.__add__(" World!"))
+
+# class Myclass
+polymorphism()
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def title(self):
+        pass
+    def presentation(self):
+        print(self.title() + self.name)
+class Man(Person):
+    def title(self):
+        return "Mr."
+class Woman(Person):
+    def title(self):
+        return "Ms."
+
+tom = Man("Tom")
+lucy = Woman("Lucy")
+for person in (tom, lucy):
+    person.presentation()
