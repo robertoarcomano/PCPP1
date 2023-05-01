7eca1974fd84e0a8db79199069cb57fe30199d00
Roberto Arcomano <info@robertoarcomano.com>
2023-05-01 15:11:38 +0200

    Abstract classes. ok

--- 1 Python-Advanced-1/1.2 OOP-Advanced/1.2.6 Abstract classes.py
+++ 1 Python-Advanced-1/1.2 OOP-Advanced/1.2.6 Abstract classes.py
@@ -1,3 +1,23 @@
 """
-
+1 Abstract classes are a contract between
+    1.1 The class designer
+    1.2 The programmer
+2 You need to import abc module
 """
+import abc
+
+class Myclass(abc.ABC):
+    def __init__(self, name):
+        self.name = name
+    @abc.abstractmethod
+    def presentation(self, *args, **kwargs):
+        pass
+    def get_name(self):
+        return self.name
+class Myclass1(Myclass):
+    def presentation(self, *args, **kwargs):
+        self.name = args[0] + " " + self.name
+
+myclass1 = Myclass1("myclass1")
+myclass1.presentation("Hello")
+print(myclass1.get_name())
