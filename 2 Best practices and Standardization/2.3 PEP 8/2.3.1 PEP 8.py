0169e9e5a694970b57f517f9c81613f752afe246
Roberto Arcomano <info@robertoarcomano.com>
2023-05-10 22:09:41 +0200

    2.2 PEP 20 - The Zen of Python. ok

---
+++ 2 Best practices and Standardization/2.3 PEP 8/2.3.1 PEP 8.py
@@ -0,0 +1,25 @@
+"""
+1 Coding convention (code style guide)
+2 Our code will be read much more often than it will be written
+3 Tools:
+    3.1 pycodestyle
+    3.2 autopep8
+    3.3 PEP 8 online
+4 Identation: 4 spaces (no tabs)
+5 Continuation lines: (opening argument functions)
+6 Max line lenght: Max 79 (maybe 99 if requested) chars, 72 for docstrings and comments
+7 Line breaks: break before operations
+8 Blank Lines (vertical whitespaces)
+    8.1 2 blank lines to surround top level fn and classes
+    8.2 1 black line to surround method definition
+    8.3 blank lines in functions to indicate logical sections
+9 Default encodings: UTF-8
+10 Imports at the beginning of the script
+    10.1 Standard library imports;
+    10.2 Related third-party imports;
+    10.3 Local application/library specific imports.
+    10.4 At least 1 blank line between groups of imports
+    10.5 Import in separated lines
+    10.6 Absolute imports
+    10.7 avoid using wildcard imports
+"""
