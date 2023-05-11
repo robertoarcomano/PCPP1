a75268039b5dbc821a25d62b8d62860d6244685f
Roberto Arcomano <info@robertoarcomano.com>
2023-05-11 11:48:00 +0200

    Going on with comments

--- 2 Best practices and Standardization/2.3 PEP 8/2.3.1 PEP 8.py
+++ 2 Best practices and Standardization/2.3 PEP 8/2.3.1 PEP 8.py
@@ -5,9 +5,9 @@
     3.1 pycodestyle
     3.2 autopep8
     3.3 PEP 8 online
-4 Identation: 4 spaces (no tabs)
+4 Indentation: 4 spaces (no tabs)
 5 Continuation lines: (opening argument functions)
-6 Max line lenght: Max 79 (maybe 99 if requested) chars, 72 for docstrings and comments
+6 Max line length: Max 79 (maybe 99 if requested) chars, 72 for docstrings and comments
 7 Line breaks: break before operations
 8 Blank Lines (vertical whitespaces)
     8.1 2 blank lines to surround top level fn and classes
@@ -22,4 +22,68 @@
     10.5 Import in separated lines
     10.6 Absolute imports
     10.7 avoid using wildcard imports
+11 Quotes:
+    11.1 Single and double are the same
+    11.2 They can mutually be used to escape characters
+12 Whitespaces:
+    12.1 Do not use unnecessary white spaces
+    12.2 No space with colon slice
+    12.3 not use excessive whitespace
+    12.4 Not more than 1 space around operators
+    12.5 One space around binary operators (+=)
+    12.6 Do not use space with "=" when it specifies a default argument
+13 Comments:
+    13.1 Use self-commenting code
+    13.2 Coherence
+    13.3 Complete sentences comment (This is a comment.)
+    13.4 2 spaces between sentences
+    13.5 Block comments:
+        13.5.1 The code just follows the comment
+        13.5.2 Same indentation as the code
 """
+import os
+import sys
+from os import sync
+var1 = {
+    "a": 1,
+    "b": "2"
+}
+var2 = (1
+        + 2
+        - 3)
+
+
+class MyClass1:
+    pass
+
+
+class MyClass2:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+def fn1():
+    print("start")
+
+    # Doing something
+    # Working on something
+
+    print("end")
+
+
+print("ok")
+a = [1, 2, 3]
+b = a[:]
+c = 1 + 2
+c += 1
+
+if a == 0:
+    # This is a comment.
+    pass
+    # This is a multi-line comment. Line 1
+    #
+    # Line 2
+
