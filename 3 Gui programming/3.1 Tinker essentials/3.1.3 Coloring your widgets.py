27f1b93e560081f52d4077782232489d0c93552e
Roberto Arcomano <info@robertoarcomano.com>
2023-05-14 22:12:40 +0200

    3.1.3 Coloring your widgets. ok

--- 3 Gui programming/3.1 Tinker essentials/3.1.3 Coloring your widgets.py
+++ 3 Gui programming/3.1 Tinker essentials/3.1.3 Coloring your widgets.py
@@ -1,3 +1,13 @@
 """
-
+1 Normal colors (bg="", fg="")
+2 Active colors (activebackground="", activeforeground="")
+3 List of colors: https://www.tcl.tk/man/tcl8.4/TkCmd/colors.html
+4 RGB: like webpages: #0000FF
 """
+import tkinter as tk
+
+
+my_window = tk.Tk()
+my_button = tk.Button(my_window, text="ok", bg="blue", fg="white", activebackground="#FF0000", activeforeground="#000000")
+my_button.pack()
+my_window.mainloop()
