b18b8a547e65b1cfa295f45d622ccf0bca132037
Roberto Arcomano <info@robertoarcomano.com>
2023-05-23 13:39:18 +0200

    3.2.4 Shaping the main window and conversing with the user.py. Going on.

--- 3 Gui programming/3.2 Lexicon of widgets and more tkinter essentials/3.2.4 Shaping the main window and conversing with the user.py
+++ 3 Gui programming/3.2 Lexicon of widgets and more tkinter essentials/3.2.4 Shaping the main window and conversing with the user.py
@@ -1,7 +1,12 @@
 """
 1 Main window is shared between the user and the operating system
     1.1 window.title()
-    1.2
+    1.2 Image
+        1.2.1 prepare an icon as a PNG image;
+        1.2.2 put the image in the same directory where the application resides;
+        1.2.3 use a PhotoImage class constructor to convert the PNG file
+              into an internal tkinter representation
+              (PhotoImage() is a part of tkinter, and we’re going to tell you more about it soon)
 """
 import tkinter as tk
 my_window = tk.Tk()
