5c14bd0f2fdabd4c4dbb13eaf0bafd6da6bfb3d7
Roberto Arcomano <info@robertoarcomano.com>
2023-05-29 12:49:57 +0200

    Added tests

---
+++ T. Tests/Use_of_User.py
@@ -0,0 +1,6 @@
+import User as My_user
+from User import user_function as my_user_function
+
+My_user.user_function()
+my_user_function()
+
---
+++ T. Tests/User.py
@@ -0,0 +1,8 @@
+# Basic User module test
+def user_function():
+    print("user_function")
+
+
+class User:
+    pass
+
