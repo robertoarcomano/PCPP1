bd46e8a4b00ee6589d8e42f5e2c6ba12fb2a4d64
Roberto Arcomano <info@robertoarcomano.com>
2023-05-30 23:13:24 +0200

    4 Working with RESTful APIs. ok.

--- 4 Working with RESTful APIs/4.6 CRUD/4.6.1 Crud.py
+++ 4 Working with RESTful APIs/4.6 CRUD/4.6.1 Crud.py
@@ -48,10 +48,11 @@ def show(json):
 
 
 def get_and_show(headers=None):
-    reply = requests.get("http://localhost:3000/cars?_sort=production_year&_order=desc", headers=headers)
+    reply = requests.get("http://localhost:3000/cars?_sort=id&_order=asc", headers=headers)
     if reply.status_code != requests.codes.ok:
         raise Exception
     show(reply.json())
+    print()
 
 
 def delete_car(car_id):
@@ -59,22 +60,35 @@ def delete_car(car_id):
 
 
 def add_car(my_new_car):
-    headers = "{'Content-Type': 'application/json'}"
-    a = json.dumps(my_new_car)
+    headers = {'Content-Type': 'application/json'}
     requests.post('http://localhost:3000/cars', headers=headers, data=json.dumps(my_new_car))
 
 
+def edit_car(id, my_car):
+    headers = {'Content-Type': 'application/json'}
+    requests.put('http://localhost:3000/cars/'+id, headers=headers, data=json.dumps(my_car))
+
+
 try:
-    # delete_car(7)
-    # exit(0)
-    # get_and_show()
     get_and_show()
-    new_car = {'id': 7,
-               'brand': 'Porsche',
-               'model': '911',
-               'production_year': 1963,
-               'convertible': False}
+    print("delete_car(7)")
+    delete_car(7)
+    get_and_show()
+    new_car = {"id": 7,
+               "brand": "Porsche",
+               "model": "911",
+               "production_year": 1963,
+               "convertible": False}
+    print("add_car(7)")
     add_car(new_car)
+    get_and_show(headers={'Connection': 'Close'})
+    new_car = {"id": 7,
+               "brand": "Porsche",
+               "model": "911",
+               "production_year": 2000,
+               "convertible": False}
+    print("edit_car(7)")
+    edit_car("7", new_car)
     get_and_show(headers={'Connection': 'Close'})
 
 except requests.RequestException:
