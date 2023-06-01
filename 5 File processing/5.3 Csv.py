88cf31551ecac27f36abf4029597668ae2e25e4a
Roberto Arcomano <info@robertoarcomano.com>
2023-06-01 22:04:49 +0200

    5.3 Csv.py Started.

--- 5 File processing/5.3 Csv.py
+++ 5 File processing/5.3 Csv.py
@@ -1,3 +1,11 @@
 """
-
+1. Csv module
+2 reader and writer objects
 """
+import csv
+
+with open("contacts.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        print(row)
+
