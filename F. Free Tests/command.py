6759ab30a1f9f2d030a7c0af541f926757e6891c
Roberto Arcomano <info@robertoarcomano.com>
2023-06-12 19:03:12 +0200

    Managed Popen

--- F. Free Tests/command.py
+++ F. Free Tests/command.py
@@ -1,5 +1,9 @@
 import subprocess
 
+def decode_and_format(string):
+    return string.decode("utf-8").replace("\n","")
+
+
 output = subprocess.run(["find", "/etc/ssh/", "/root"], capture_output=True)
 print(type(output.stdout))
 print(output.stdout.decode("utf-8"))
@@ -9,3 +13,11 @@ output = subprocess.Popen(["find", "/etc/ssh"], stdout=subprocess.PIPE)
 print(type(output.stdout))
 print(output.stdout.read().decode("utf-8"))
 print(output.stderr)
+print()
+
+output = subprocess.Popen(["find", "/etc/ssh"], stdout=subprocess.PIPE)
+output1 = subprocess.Popen(["grep", "config"], stdout=subprocess.PIPE, stdin = output.stdout)
+for line in output1.stdout.readlines():
+    my_dir = decode_and_format(line)
+    dir_with_size = decode_and_format(subprocess.check_output(["du", "-s", "-h", my_dir]))
+    print(dir_with_size)
--- F. Free Tests/file.py
+++ F. Free Tests/file.py
@@ -10,16 +10,11 @@ def all_replaces(string):
     return remove_lf(replace_quotes(string))
 
 
-def show(my_list):
-    for item in my_list:
-        print(item)
+def read_and_format_file(filename):
+    with open("file.py", "r") as my_file:
+        return list(map(all_replaces, my_file.readlines()))
 
 
-my_file = open("file.py", "r")
-my_data1 = my_file.readlines()
-my_file.close()
-
-my_data3 = list(map(all_replaces, my_data1))
-show(my_data1)
-show(my_data3)
+for item in read_and_format_file("file.py"):
+    print(item)
 
