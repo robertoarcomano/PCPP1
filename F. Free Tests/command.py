d60218d12b8cbd3d5036d70caffc60dacb1a4446
Roberto Arcomano <info@robertoarcomano.com>
2023-06-12 19:09:14 +0200

    Improved.

--- F. Free Tests/command.py
+++ F. Free Tests/command.py
@@ -1,13 +1,20 @@
 import subprocess
 
-def decode_and_format(string):
+def chop(string):
+    return string.replace("\n","")
+
+def decode(string):
+    return string.decode("utf-8")
+
+def decode_and_chop(string):
     return string.decode("utf-8").replace("\n","")
 
 
 output = subprocess.run(["find", "/etc/ssh/", "/root"], capture_output=True)
 print(type(output.stdout))
-print(output.stdout.decode("utf-8"))
-print(output.stderr.decode("utf-8"))
+print(decode(output.stdout))
+print(decode(output.stderr))
+print()
 
 output = subprocess.Popen(["find", "/etc/ssh"], stdout=subprocess.PIPE)
 print(type(output.stdout))
@@ -18,6 +25,6 @@ print()
 output = subprocess.Popen(["find", "/etc/ssh"], stdout=subprocess.PIPE)
 output1 = subprocess.Popen(["grep", "config"], stdout=subprocess.PIPE, stdin = output.stdout)
 for line in output1.stdout.readlines():
-    my_dir = decode_and_format(line)
-    dir_with_size = decode_and_format(subprocess.check_output(["du", "-s", "-h", my_dir]))
+    my_dir = decode_and_chop(line)
+    dir_with_size = decode_and_chop(subprocess.check_output(["du", "-s", "-h", my_dir]))
     print(dir_with_size)
