281d38116254a17e48b08473cbf4f12da8071a82
Roberto Arcomano <info@robertoarcomano.com>
2023-06-13 11:18:27 +0200

    Added stack.py

--- F. Free Tests/class.py
+++ F. Free Tests/class.py
@@ -1,8 +1,32 @@
+import json
+
+
+def show_types(obj):
+    var = globals()[obj]
+    print("type({}):".format(obj), type(var), " | {}.__class__:".format(obj), var.__class__)
+
+
 class MyClass:
+    '''My __init__ method'''
     def __init__(self, name):
         self.name = name
 
+
 my_object = MyClass("Roberto")
-print(getattr(my_object, "name"))
+print("getattr(my_object, \"name\"):", getattr(my_object, "name"))
+print("setattr(my_object, \"name\", \"Roby\")")
 setattr(my_object, "name", "Roby")
-print(getattr(my_object, "name"))
+print("getattr(my_object, \"name\"):", getattr(my_object, "name"))
+print()
+
+show_types("my_object")
+show_types("MyClass")
+show_types("__name__")
+""" my variable a"""
+a = 2
+show_types("a")
+print()
+
+print("MyClass.__dict__:", MyClass.__dict__)
+print("MyClass.__doc__:", MyClass.__doc__)
+print("a.__doc__:", a.__doc__)
---
+++ F. Free Tests/stack.py
@@ -0,0 +1,12 @@
+my_stack = [1, 2, 4]
+print("my_stack:", my_stack)
+print()
+
+print("my_stack.append(10)")
+my_stack.append(10)
+print("my_stack:", my_stack)
+print()
+
+print("my_stack..pop()")
+print(my_stack.pop())
+print("my_stack:", my_stack)
