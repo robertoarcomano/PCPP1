f4b8a704173b6041aeedbbb685f5ee7fd2bfba0e
Roberto Arcomano <info@robertoarcomano.com>
2023-06-14 17:37:32 +0200

    Added @staticmethod

--- F. Free Tests/class.py
+++ F. Free Tests/class.py
@@ -6,15 +6,22 @@ def show_types(obj):
     print("type({}):".format(obj), type(var), " | {}.__class__:".format(obj), var.__class__)
 
 
-
 class MyClass:
     '''My __init__ method'''
+
     def __init__(self, name):
         self.name = name
         print(global_var)
 
+    @staticmethod
+    def my_static():
+        print(global_var)
+        return False
+
+
 global_var = "ok"
 my_object = MyClass("Roberto")
+my_object.my_static()
 print("getattr(my_object, \"name\"):", getattr(my_object, "name"))
 print("setattr(my_object, \"name\", \"Roby\")")
 setattr(my_object, "name", "Roby")
