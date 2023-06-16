c4041e3b6487b4fbe1e44fa0b136b8c01a22dfc2
Roberto Arcomano <info@robertoarcomano.com>
2023-06-16 08:42:57 +0200

    Added conversions.py

---
+++ F. Free Tests/conversions.py
@@ -0,0 +1,14 @@
+r = 10.6
+print("r (float):", r)
+
+n = int(r)
+print("int(r):", n)
+
+h = hex(n)
+print("hex(n):", hex(n))
+
+f = float(n)
+print("float(n):", f)
+
+o = oct(n)
+print("oct(n):", o)
