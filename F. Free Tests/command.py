235ddfede8ad28c26f2335aa694c453df435435b
Roberto Arcomano <info@robertoarcomano.com>
2023-06-19 09:57:43 +0200

    files structure changed1

--- F. Free Tests/command.py
+++ F. Free Tests/command.py
@@ -1,13 +1,16 @@
 import subprocess
 
+
 def chop(string):
-    return string.replace("\n","")
+    return string.replace("\n", "")
+
 
 def decode(string):
     return string.decode("utf-8")
 
+
 def decode_and_chop(string):
-    return string.decode("utf-8").replace("\n","")
+    return string.decode("utf-8").replace("\n", "")
 
 
 output = subprocess.run(["find", "/etc/ssh/", "/root"], capture_output=True)
@@ -23,7 +26,7 @@ print(output.stderr)
 print()
 
 output = subprocess.Popen(["find", "/etc/ssh"], stdout=subprocess.PIPE)
-output1 = subprocess.Popen(["grep", "config"], stdout=subprocess.PIPE, stdin = output.stdout)
+output1 = subprocess.Popen(["grep", "config"], stdout=subprocess.PIPE, stdin=output.stdout)
 for line in output1.stdout.readlines():
     my_dir = decode_and_chop(line)
     dir_with_size = decode_and_chop(subprocess.check_output(["du", "-s", "-h", my_dir]))
---
+++ F. Free Tests/docs_type_is_conversions.py
@@ -0,0 +1,83 @@
+def title(string, *args):
+    def header():
+        title_char = "#"
+        title_char_number = 3
+        for h in range(title_char_number):
+            print(title_char, end="")
+
+    header()
+    print(" " + string, end=" ")
+    header()
+    print()
+    for arg in args:
+        print(arg)
+    if args:
+        print()
+
+
+my_var = 5
+my_string = "ok"
+
+
+class Base:
+    pass
+
+
+class MyClass(Base):
+    static_var = 8
+
+    def __init__(self):
+        pass
+
+
+def fn(a=True):
+    """fn function"""
+    pass
+
+
+my_obj = MyClass()
+text_number = 1
+title("fn.__doc__", fn.__doc__)
+
+title("help(fn)")
+help(fn)
+print()
+
+title("dir()", dir())
+
+title("type(var)", type(my_var))
+
+title("type(string)", type(my_string))
+
+title("type(fn)", type(fn))
+
+title("type(MyClass)", type(MyClass))
+
+title("type(MyClass.static_var)", type(MyClass.static_var))
+
+title("type(MyClass.__init__)", type(MyClass.__init__))
+
+title("isinstance(my_obj, Base):", isinstance(my_obj, Base))
+
+title("issubclass(MyClass, Base):", issubclass(MyClass, Base))
+
+real = 10.6
+title("real [float]:", real)
+
+n = int(real)
+title("n = int(real):", n)
+
+h = hex(n)
+title("hex(n):", hex(n))
+
+f = float(n)
+title("float(n):", f)
+
+o = oct(n)
+title("oct(n):", o)
+
+b = bool(n)
+title("b = bool(n):", b)
+
+s = str(b)
+title("str(b):", s)
