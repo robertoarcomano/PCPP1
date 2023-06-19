5c6fe63df80ff104329cdbb2c92f8e9325fc6935
Roberto Arcomano <info@robertoarcomano.com>
2023-06-19 10:08:01 +0200

    files structure changed 2

--- F. Free Tests/docs_type_is_conversions.py
+++ F. Free Tests/docs_type_is_conversions.py
@@ -57,27 +57,36 @@ title("type(MyClass.static_var)", type(MyClass.static_var))
 
 title("type(MyClass.__init__)", type(MyClass.__init__))
 
-title("isinstance(my_obj, Base):", isinstance(my_obj, Base))
+title("isinstance(my_obj, Base)", isinstance(my_obj, Base))
 
-title("issubclass(MyClass, Base):", issubclass(MyClass, Base))
+title("issubclass(MyClass, Base)", issubclass(MyClass, Base))
 
 real = 10.6
-title("real [float]:", real)
+title("real [float]", real)
 
 n = int(real)
-title("n = int(real):", n)
+title("n = int(real)", n)
 
 h = hex(n)
-title("hex(n):", hex(n))
+title("hex(n)", hex(n))
 
 f = float(n)
-title("float(n):", f)
+title("float(n)", f)
 
 o = oct(n)
-title("oct(n):", o)
+title("oct(n)", o)
 
 b = bool(n)
-title("b = bool(n):", b)
+title("b = bool(n)", b)
 
 s = str(b)
-title("str(b):", s)
+title("str(b)", s)
+
+print("a = [1, 2, 3]")
+print("b = [1, 2, 3]")
+print()
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+title("a == b", a == b)
+title("a is b", a is b)
