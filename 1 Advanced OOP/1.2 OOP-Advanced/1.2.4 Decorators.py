35dfeca551d445a580a212fda6ed8ae835b3cdeb
Roberto Arcomano <info@robertoarcomano.com>
2023-06-20 11:47:28 +0200

    Added datatype.py

--- 1 Advanced OOP/1.2 OOP-Advanced/1.2.4 Decorators.py
+++ 1 Advanced OOP/1.2 OOP-Advanced/1.2.4 Decorators.py
@@ -39,29 +39,41 @@ def decorator_new(function):
         print("pre", function.__name__)
         function(*args, **kwargs)
         print("post", function.__name__)
+
     return wrapper
+
+
 @decorator_new
 def myfunction_new(*args, **kwargs):
     print("myfunction_new. args:", args, "kwargs:", kwargs)
 
-myfunction_new(1,2, a=1)
+
+myfunction_new(1, 2, a=1)
 print()
+
 
 def generic_decorator(*decorator_args, **decorator_kwargs):
     def external_wrapper(function):
         print("generic_decorator. args:", decorator_args, "kwargs:", decorator_kwargs)
+
         def internal_wrapper(*args, **kwargs):
             print("pre", function.__name__)
             function(*args, **kwargs)
             print("post", function.__name__)
+
         return internal_wrapper
+
     return external_wrapper
+
+
 @generic_decorator(2, b=3)
 def myfunction_with_generic_decorator(*args, **kwargs):
     print("myfunction_with_generic_decorator. args:", args, "kwargs:", kwargs)
 
+
 myfunction_with_generic_decorator(1, b=2)
 print()
+
 
 def generic_decorator_outer(*decorator_args, **decorator_kwargs):
     def external_wrapper(function):
@@ -69,74 +81,97 @@ def generic_decorator_outer(*decorator_args, **decorator_kwargs):
             print("outer.pre", function.__name__)
             function(*args, **kwargs)
             print("outer.post", function.__name__)
+
         return internal_wrapper
+
     return external_wrapper
+
+
 def generic_decorator_inner(*decorator_args, **decorator_kwargs):
     def external_wrapper(function):
         def internal_wrapper(*args, **kwargs):
             print("inner.pre", function.__name__)
             function(*args, **kwargs)
             print("inner.post", function.__name__)
+
         return internal_wrapper
+
     return external_wrapper
+
+
 @generic_decorator_outer(1, b=2)
 @generic_decorator_inner(3, b=4)
 def myfunction_with_generic_stacked_decorators(*args, **kwargs):
     print("myfunction_with_generic_decorator. args:", args, "kwargs:", kwargs)
 
+
 myfunction_with_generic_stacked_decorators(5, b=6)
 print()
+
 
 class ClassDecorating:
     def __init__(self, function):
         self.function = function
+
     def __call__(self, *args, **kwargs):
         print("ClassDecorating.pre. args:", args, " kwargs:", kwargs)
         self.function(*args, **kwargs)
         print("ClassDecorating.post. args:", args, " kwargs:", kwargs)
 
+
 @ClassDecorating
 def myfunction_with_class_decorating(*args, **kwargs):
     print("myfunction_with_class_decorating. args:", args, "kwargs:", kwargs)
 
+
 myfunction_with_class_decorating(1, b=2)
 print()
+
 
 class ClassDecoratingWithArguments:
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
+
     def __call__(self, function):
         def wrapper(*args, **kwargs):
             print("ClassDecoratingWithArguments.pre. args:", args, " kwargs:", kwargs)
             function(*args, **kwargs)
             print("ClassDecoratingWithArguments.post. args:", args, " kwargs:", kwargs)
+
         return wrapper
+
 
 @ClassDecoratingWithArguments(3, c=2)
 def myfunction_with_class_decorating_with_arguments(*args, **kwargs):
     print("myfunction_with_class_decorating_with_arguments. args:", args, "kwargs:", kwargs)
 
+
 myfunction_with_class_decorating_with_arguments(1, b=2)
 print()
 
+
 def myfunction_decorator_of_class(class_):
     class_.__old_getattribute__ = class_.__getattribute__
+
     def new_getattr(self, name):
         if name == "name":
             print("name is read")
         return class_.__old_getattribute__(self, name)
+
     class_.__getattribute__ = new_getattr
     return class_
-@myfunction_decorator_of_class
 
+
+@myfunction_decorator_of_class
 class ClassDecorator:
     def __init__(self, name):
         self.name = name
+
     def get_name(self):
         return self.name
+
 
 class_decorator = ClassDecorator("class_decorator")
 class_decorator.get_name()
 print()
-
---
+++ F. Free Tests/datatype.py
@@ -0,0 +1,46 @@
+from common import title
+
+my_list = [1, 2, 3, 3]
+title("my_list", my_list)
+print("my_list.append(4)")
+my_list.append(4)
+title("my_list", my_list)
+
+my_tuple = 1, 2, 3, 3, "Hello!", [10, 20, 30]
+title("my_tuple", my_tuple)
+
+title("my_tuple[5]", my_tuple[5])
+print("my_tuple[5][0] = 4")
+my_tuple[5][0] = 4
+title("my_tuple[5]", my_tuple[5])
+title("my_tuple", my_tuple)
+
+print("s = set(my_list)")
+s = set(my_list)
+title("s", s)
+print("s.add(2)")
+s.add(2)
+title("s", s)
+print("s.add(5)")
+s.add(5)
+title("s", s)
+print("s.pop()")
+s.pop()
+title("s", s)
+
+print("f = frozenset(s.union((9,)))")
+f = frozenset(s.union((9,)))
+title("f")
+for i in f:
+    print(i, end=" ")
+print()
+print()
+print("c = complex(1, 2)")
+c = complex(1, 2)
+title("c", c)
+
+print("r = range(10, 0, -1)")
+r = range(10, 0, -1)
+title("r")
+for i in r:
+    print(i, end=" ")
