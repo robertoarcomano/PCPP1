0fad205320925da49e97a684545cd9f413c8bf01
Roberto Arcomano <info@robertoarcomano.com>
2023-06-20 16:33:52 +0200

    decorators.py updated

--- F. Free Tests/decorators.py
+++ F. Free Tests/decorators.py
@@ -5,28 +5,50 @@ import time
 def add_mr(func):
     def wrapper(name):
         start = time.perf_counter()
-        s = "before\n"
-        s += "   " + func("Mr." + name)
+        s = "before |"
+        s += func("Mr." + name)
         time.sleep(0.1)
-        s += "\nafter\n"
         stop = time.perf_counter()
-        s += "\n"
-        s += str(stop - start) + " seconds"
+        s += "| after |" + str(stop - start) + " seconds"
         return s
 
     return wrapper
 
 
-def greeting1(name):
+def add_params(person_title):
+    def external_wrapper(func):
+        def internal_wrapper(name):
+            start = time.perf_counter()
+            s = "before |"
+            s += func(person_title + name)
+            time.sleep(0.1)
+            stop = time.perf_counter()
+            s += "| after |" + str(stop - start) + " seconds"
+            return s
+
+        return internal_wrapper
+
+    return external_wrapper
+
+
+def greeting(name):
     return "Hello " + name
 
 
 @add_mr
-def greeting2(name):
+def greeting_auto(name):
     return "Hello " + name
 
 
-greeting1_d = add_mr(greeting1)
-title("greeting1_d('Roby')", greeting1_d('Roby'))
+@add_params("Ms.")
+def greeting_params_auto(name):
+    return "Hello " + name
 
-title("greeting2('Roby')", greeting2('Roby'))
+
+greeting_manual = add_mr(greeting)
+title("greeting_manual('Roby')", greeting_manual('Roby'))
+title("greeting_auto('Roby')", greeting_auto('Roby'))
+
+greeting_params_manual = add_params("Ms.")(greeting)
+title("greeting_params_manual('Roby')", greeting_params_manual('Roby'))
+title("greeting_params_auto('Roby')", greeting_params_auto('Roby'))
